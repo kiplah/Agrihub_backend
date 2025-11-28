4ef8ffdc6ceb4b30cba377a9a6843a93fbe2f1db
kiplah <kiplahvictor27@gmail.com>
2025-11-28 21:54:37 +0300

    fixed

--- agromart/settings.py
+++ agromart/settings.py
@@ -128,3 +128,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 AUTH_USER_MODEL = 'api.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
