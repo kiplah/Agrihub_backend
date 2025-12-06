d957838b6f8f5f8b6b0294531c526daf2dc2c021
kiplah <kiplahvictor27@gmail.com>
2025-12-06 13:42:25 +0300

    Updated settings, urls, product model and added new product migration

--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 class ProductCategory(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    imagepath = models.CharField(max_length=255, blank=True, null=True)
+    imagepath = models.ImageField(upload_to='categories/', blank=True, null=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='categories')
 
     def __str__(self):
@@ -13,7 +13,7 @@ class ProductCategory(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    imagepath = models.CharField(max_length=255, blank=True, null=True)
+    imagepath = models.ImageField(upload_to='products/', blank=True, null=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='products')
     category_name = models.CharField(max_length=255, blank=True, null=True) # Keeping as string to match Go, or could link to ProductCategory
     price = models.BigIntegerField()
