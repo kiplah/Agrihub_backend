52117ce07104606a19a5e80311bed2316b102954
kiplah <kiplahvictor27@gmail.com>
2025-12-13 16:09:51 +0300

    feat: add product and product category serializers with nested subcategories and custom category creation logic.

--- products/serializers.py
+++ products/serializers.py
@@ -15,6 +15,7 @@ class ProductCategorySerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     category_details = ProductCategorySerializer(source='category', read_only=True)
     user = serializers.ReadOnlyField(source='user.username')
+    user_id = serializers.IntegerField(source='user.id', read_only=True)
     category_name = serializers.CharField(write_only=True, required=False)
     
     class Meta:
