1bfd56155d684efa5c576e13b5f733d5e4bb73c1
kiplah <kiplahvictor27@gmail.com>
2025-12-27 13:06:15 +0300

    feat: Add initial Django project settings including CORS, REST Framework, and custom app configurations.

--- agromart/settings.py
+++ agromart/settings.py
@@ -138,7 +138,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+CORS_ALLOW_CREDENTIALS = True
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
